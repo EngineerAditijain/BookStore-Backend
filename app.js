7daf057b312c69f420829ac38d6a0140a1200ae2
Aditi Jain <94610360+EngineerAditijain@users.noreply.github.com>
2023-01-19 17:05:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 
-//qWwxiognwrFERDK3
-/*const express=require('express');
-const mongooose=require('mongoose');
-const router=require("./routes/book-routes");
-const cors=require('cors')
-const app=express();
-
-app.use(express.json());
-app.use("/books",router);
-app.use(cors());
-
-mongooose.connect("mongodb+srv://Aditi_data:qWwxiognwrFERDK3@cluster0.9lvyu.mongodb.net/Bookstore?retryWrites=true&w=majority")
-.then(()=>console.log("connected to database"))
-.then(()=>{
-    app.listen(5000,)
-}).catch((err)=>console.log(err));*/
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
@@ -39,4 +23,3 @@ mongoose
     app.listen(PORT);
   })
   .catch((err) => console.log(err));
-
